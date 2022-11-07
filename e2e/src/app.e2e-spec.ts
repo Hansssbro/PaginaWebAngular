74f5d0876b24730a7c35a30516602889a42becc5
Your Name <you@example.com>
2022-11-07 08:24:49 -0500

    commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('MiprimeraPagina app is running!');
+    expect(page.getTitleText()).toEqual('pagina app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MiprimeraPagina'`, () => {
+  it(`should have as title 'pagina'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MiprimeraPagina');
+    expect(app.title).toEqual('pagina');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MiprimeraPagina app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pagina app is running!');
   });
 });
